5c36b174e2de695f04bded3bbae9d2aded46cf73
Walter Gomero <walter.gomero@hp.com>
2018-08-05 22:30:14 -0500

    Update usp_LeaseAddProperties stored procedures. Inserted active tenants to the lease table.

--- dbRentalProperty/dbo/Stored Procedures/usp_LeaseAddProperties.sql
+++ dbRentalProperty/dbo/Stored Procedures/usp_LeaseAddProperties.sql
@@ -14,26 +14,27 @@ BEGIN
 		FROM STRING_SPLIT(@p_chrPropertyIds, ',')  
 			WHERE RTRIM(value) <> '';  
 
-	DECLARE @Id INT, @Count INT;
+	DECLARE @Id INT, @Count INT, @TenantId INT;
 	SELECT @Count = COUNT(*) FROM @PropertyList;
 
 	WHILE @Count > 0
 		BEGIN
 			SELECT TOP 1 @Id = PropertyId FROM @PropertyList WHERE Inserted IS NULL;
+			SELECT @TenantId = ISNULL(TenantId, 0) FROM Tenant WHERE PropertyID = @Id AND StatusID = 3 -- Get TenantId from Tenant if Tenant is Active Tenant
 				IF NOT EXISTS (SELECT 1 FROM Lease WHERE PropertyID = @Id AND [Year] = @p_intYear)
 					BEGIN
-						INSERT INTO Lease(PropertyId, [Year], [Month]) VALUES(@Id, @p_intYear, 1);
-						INSERT INTO Lease(PropertyId, [Year], [Month]) VALUES(@Id, @p_intYear, 2);
-						INSERT INTO Lease(PropertyId, [Year], [Month]) VALUES(@Id, @p_intYear, 3);
-						INSERT INTO Lease(PropertyId, [Year], [Month]) VALUES(@Id, @p_intYear, 4);
-						INSERT INTO Lease(PropertyId, [Year], [Month]) VALUES(@Id, @p_intYear, 5);
-						INSERT INTO Lease(PropertyId, [Year], [Month]) VALUES(@Id, @p_intYear, 6);
-						INSERT INTO Lease(PropertyId, [Year], [Month]) VALUES(@Id, @p_intYear, 7);
-						INSERT INTO Lease(PropertyId, [Year], [Month]) VALUES(@Id, @p_intYear, 8);
-						INSERT INTO Lease(PropertyId, [Year], [Month]) VALUES(@Id, @p_intYear, 9);
-						INSERT INTO Lease(PropertyId, [Year], [Month]) VALUES(@Id, @p_intYear, 10);
-						INSERT INTO Lease(PropertyId, [Year], [Month]) VALUES(@Id, @p_intYear, 11);
-						INSERT INTO Lease(PropertyId, [Year], [Month]) VALUES(@Id, @p_intYear, 12);
+						INSERT INTO Lease(PropertyId, TenantId, [Year], [Month]) VALUES(@Id, @TenantId, @p_intYear, 1);
+						INSERT INTO Lease(PropertyId, TenantId, [Year], [Month]) VALUES(@Id, @TenantId, @p_intYear, 2);
+						INSERT INTO Lease(PropertyId, TenantId, [Year], [Month]) VALUES(@Id, @TenantId, @p_intYear, 3);
+						INSERT INTO Lease(PropertyId, TenantId, [Year], [Month]) VALUES(@Id, @TenantId, @p_intYear, 4);
+						INSERT INTO Lease(PropertyId, TenantId, [Year], [Month]) VALUES(@Id, @TenantId, @p_intYear, 5);
+						INSERT INTO Lease(PropertyId, TenantId, [Year], [Month]) VALUES(@Id, @TenantId, @p_intYear, 6);
+						INSERT INTO Lease(PropertyId, TenantId, [Year], [Month]) VALUES(@Id, @TenantId, @p_intYear, 7);
+						INSERT INTO Lease(PropertyId, TenantId, [Year], [Month]) VALUES(@Id, @TenantId, @p_intYear, 8);
+						INSERT INTO Lease(PropertyId, TenantId, [Year], [Month]) VALUES(@Id, @TenantId, @p_intYear, 9);
+						INSERT INTO Lease(PropertyId, TenantId, [Year], [Month]) VALUES(@Id, @TenantId, @p_intYear, 10);
+						INSERT INTO Lease(PropertyId, TenantId, [Year], [Month]) VALUES(@Id, @TenantId, @p_intYear, 11);
+						INSERT INTO Lease(PropertyId, TenantId, [Year], [Month]) VALUES(@Id, @TenantId, @p_intYear, 12);
 
 						UPDATE @PropertyList SET Inserted = 'YES' WHERE PropertyId = @Id
 					END
@@ -59,4 +60,3 @@ BEGIN
 		END 
 END
 GO
-
